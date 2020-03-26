675be62faacccf10671759d364300b8e15dbe610
alex-bonetti <62677325+alex-bonetti@users.noreply.github.com>
2020-03-26 10:40:45 -0300

    Criação do arquivo de rotas do App e criação dos arquivos restantes para as telas

---
+++ routes/StackRoutes.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import {createStackNavigator} from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+
+import Login from "../src/screens/login";
+import Cadastro from "../src/screens/cadastro";
+import Home from "../src/screens/home";
+import CarregarLogin from "../src/screens/carregarLogin";
+import TelaInicial from "../src/screens/telaInicial";
+import AddReceita from "../src/screens/addReceita"; 
+import AddDespesa from "../src/screens/addDespesa";
+import Historico from "../src/screens/historico";
+
+export default function MyStack(){
+    return(
+        <Stack.Navigator>
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Cadastro" component={Cadastro} />
+            <Stack.Screen name="TelaInicial" component={TelaInicial} />
+            <Stack.Screen name="CarregarLogin" component={CarregarLogin} />
+            <Stack.Screen name="AddDespesa" component={AddDespesa} />
+            <Stack.Screen name="AddReceita" component={AddReceita} />
+            <Stack.Screen name="Historico" component={Historico} />
+        </Stack.Navigator>
+    )
+}
